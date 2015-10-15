c948d854bac762ce7092a8dffdfbde52cd62afbb
Leny BERNARD <leny@appventus.com>
2015-10-15 10:53:36 -0400

    Applied fixes from StyleCI

--- Form/WidgetSitemapType.php
+++ Form/WidgetSitemapType.php
@@ -6,44 +6,44 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Victoire\Bundle\CoreBundle\Form\WidgetType;
 
-
 /**
- * WidgetSitemap form type
+ * WidgetSitemap form type.
  */
 class WidgetSitemapType extends WidgetType
 {
     /**
-     * define form fields
+     * define form fields.
+     *
      * @param FormBuilderInterface $builder
+     *
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('rootPage', null, array(
-            'label' => 'widget_sitemap.form.rootPage.label'
-        ));
+        $builder->add('rootPage', null, [
+            'label' => 'widget_sitemap.form.rootPage.label',
+        ]);
         parent::buildForm($builder, $options);
-
     }
 
-
     /**
-     * bind form to WidgetSitemap entity
+     * bind form to WidgetSitemap entity.
+     *
      * @param OptionsResolverInterface $resolver
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         parent::setDefaultOptions($resolver);
 
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'data_class'         => 'Victoire\Widget\SitemapBundle\Entity\WidgetSitemap',
             'widget'             => 'Sitemap',
-            'translation_domain' => 'victoire'
-        ));
+            'translation_domain' => 'victoire',
+        ]);
     }
 
     /**
-     * get form name
+     * get form name.
      *
      * @return string The form name
      */
